3ef160ec5bc206321f59cd24568f63c3e69d0dc7
cdc <96666783+cdc-sys@users.noreply.github.com>
2024-03-06 20:09:38 +0100

    js

---
+++ ui.js
@@ -0,0 +1,58 @@
+pages = 0
+page_contents = []
+page_array = []
+async function getThumbsJSON(){
+    x = await fetch("https://raw.githubusercontent.com/cdc-sys/level-thumbnails/main/thumbnails.json")
+    y = await x.text()
+    z = JSON.parse(y)
+    return z
+}
+theJSON = await getThumbsJSON();
+function setupPages(){
+    page_contents = []
+    page_array = []
+    id = 0;
+    for (var i = 0; i<pages;i++){
+        for (var i2 = 0; i2<25;i2++){
+            page_contents.push(theJSON.level_ids[id])
+            id+=1;
+        }
+        page_array.push(page_contents)
+                page_contents = []
+    }
+}
+function renderPage(index){
+    renderingPage = page_array[index]
+    div = document.createElement("div")
+    div.id = "page-view"
+    for (img of renderingPage){
+        image = document.createElement("img")
+        image.src = `https://cdc-sys.github.io/level-thumbnails/thumbs/${img}.png`
+        image.width = 256
+        image.height = 144
+        div.appendChild(image)
+    }
+    document.body.appendChild(div)
+}
+function renderPageButtons(){
+    existingButtonDiv = document.getElementById("button-view")
+    if (existingButtonDiv != undefined){
+        document.body.removeChild(existingButtonDiv)
+    }
+    div = document.createElement("div")
+    div.id = "button-view"
+    for (page in page_array){
+        button = document.createElement("button")
+        button.innerText = parseInt(page) + 1;
+        button.setAttribute("onclick",`renderWithPage(${page});`)
+        div.appendChild(button)
+    }
+    document.body.appendChild(div)
+}
+
+function renderWithPage(index){
+    document.body.innerHTML = ""
+    renderPage(index)
+    renderPageButtons();
+}
+renderWithPage(0)
